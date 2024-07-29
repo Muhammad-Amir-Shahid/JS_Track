6bf06c28abb6fd90339e0dc4b913e6a24c1bf84d
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-29 23:15:48 +0500

    Arithmetic Opeators

--- operators.js
+++ operators.js
@@ -5,20 +5,20 @@ comments*/
 
 let a=5;
 let b=3;
-// let c=a+b;
-// console.log("a =", a ,"b =", b);
-// console.log("a + b = ", c);
-// console.log("a - b = ",a-b );
-// console.log("a * b = ",a*b );
-// console.log("a / b = ",a/b );
-// console.log("a % b = ",a%b);
-// console.log("a ** b = ",a**b);// exponentiation
-// //unary operator 
+ let c=a+b;
+ console.log("a =", a ,"b =", b);
+ console.log("a + b = ", c);
+ console.log("a - b = ",a-b );
+ console.log("a * b = ",a*b );
+ console.log("a / b = ",a/b );
+ console.log("a % b = ",a%b);
+ console.log("a ** b = ",a**b);// exponentiation
+ //unary operator 
 
-// console.log("a =", ++a);
-// console.log("a==b", a==b);
-// let d= 6;
-// console.log("a!==d", a!==d);
+ console.log("a =", ++a);
+ console.log("a==b", a==b);
+ let d= 6;
+ console.log("a!==d", a!==d);
 
 
 // logical operators
