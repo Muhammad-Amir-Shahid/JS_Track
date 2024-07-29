c04b976331334c0d6bf71c5cd94e72c0955af8ce
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-29 23:12:05 +0500

    a

--- operators.js
+++ operators.js
@@ -5,18 +5,28 @@ comments*/
 
 let a=5;
 let b=3;
-let c=a+b;
-console.log("a =", a ,"b =", b);
-console.log("a + b = ", c);
-console.log("a - b = ",a-b );
-console.log("a * b = ",a*b );
-console.log("a / b = ",a/b );
-console.log("a % b = ",a%b);
-console.log("a ** b = ",a**b);// exponentiation
-//unary operator 
+// let c=a+b;
+// console.log("a =", a ,"b =", b);
+// console.log("a + b = ", c);
+// console.log("a - b = ",a-b );
+// console.log("a * b = ",a*b );
+// console.log("a / b = ",a/b );
+// console.log("a % b = ",a%b);
+// console.log("a ** b = ",a**b);// exponentiation
+// //unary operator 
 
-console.log("a =", ++a);
-console.log("a==b", a==b);
-let d= 6;
-console.log("a!==d", a!==d);
+// console.log("a =", ++a);
+// console.log("a==b", a==b);
+// let d= 6;
+// console.log("a!==d", a!==d);
+
+
+// logical operators
+
+let condition1=a>b;
+let condition2=a===5;
+
+console.log(condition1 && condition2);
+console.log(!a>b);
+
 
