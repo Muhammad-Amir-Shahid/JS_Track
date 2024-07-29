b53cfde2456379feb69545ff4f553ac5dad41b52
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-29 23:33:37 +0500

    condition operatorss

---
+++ conditional statement.js
@@ -0,0 +1,19 @@
+conditional statement
+let age=25;
+if (age>=18)
+{
+    console.log("cam vote");
+}
+
+
+let mode = "dark";
+let color;
+if (mode==="dark")
+{
+    color ="black";
+}
+if (mode==="light")
+{
+    color="white";
+}
+console.log(color);
