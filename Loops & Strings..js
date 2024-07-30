86dacaa5b22a5be379c82a5b02ec4b8fb3eed479
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-30 22:52:11 +0500

    All loops

--- Loops & Strings..js
+++ Loops & Strings..js
@@ -1,35 +1,35 @@
-// //for loop
-// let sum = 0;
-// for (let i=0;i<5;i++)
-// {
-//   sum = sum + i;
-// }
-// console.log(sum);
+//for loop
+let sum = 0;
+for (let i=0;i<5;i++)
+{
+  sum = sum + i;
+}
+console.log(sum);
 
-// //while loop
-// let j=0;
-// while(j<5)
-// {
-//     console.log(j);
-//     j++;
-// }
+//while loop
+let j=0;
+while(j<5)
+{
+    console.log(j);
+    j++;
+}
 
-// //Do while loop
-// let k=0;
-// do {
-// console.log(k);
-// k++;
-// }while(k<5);
+//Do while loop
+let k=0;
+do {
+console.log(k);
+k++;
+}while(k<5);
 
 
-// //for of Loop
-// let str = "Amir Khan";
-// for(let l of str)
-// {
-//     console.log(l);
-// }
+//for of Loop
+let str = "Amir Khan";
+for(let l of str)
+{
+    console.log(l);
+}
 
-// //for in loop
+//for in loop
 
 let student ={
 name:"Amir Khan",
