80b73f8fefa0113f4471bb40df73bd915c4b6909
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-30 22:36:11 +0500

    all loops

--- Loops & Strings..js
+++ Loops & Strings..js
@@ -5,3 +5,19 @@ for (let i=0;i<5;i++)
   sum = sum + i;
 }
 console.log(sum);
+
+let j=0;
+while(j<5)
+{
+    console.log(j);
+    j++;
+}
+
+let k=0;
+do {
+console.log(k);
+k++;
+}while(k<5);
+
+
+
