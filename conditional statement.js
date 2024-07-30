28fc16487a9148c7df9c4ee2a24a89bbbb5d7873
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-30 21:06:05 +0500

    ..

--- conditional statement.js
+++ conditional statement.js
@@ -1,25 +1,44 @@
-//conditional statement
-let age=25;
-if (age>=18)
+// //conditional statement
+// let age=25;
+// if (age>=18)
+// {
+//     console.log("can vote");
+// }
+
+// let result=age>18 ? "Yes":"No";  
+// console.log(result);
+
+// let mode = "dark";
+// let color;
+// if (mode==="dark")
+// {
+//     color ="black";
+// }
+// else if (mode==="light")
+// {
+//     color="white";
+// }
+// else
+// {
+// color ="Blue";
+// }
+// console.log(color);
+const expr="orange";
+switch(expr)
 {
-    console.log("can vote");
+case 'orange':
+    {
+        console.log("Orange");
+        break;
+    }
+case "Potato":
+    {
+        break;
+    }
+case "Egg":
+    {
+        break;
+    }
+
 }
 
-let result=age>18 ? "Yes":"No";  
-console.log(result);
-
-let mode = "dark";
-let color;
-if (mode==="dark")
-{
-    color ="black";
-}
-else if (mode==="light")
-{
-    color="white";
-}
-else
-{
-color ="Blue";
-}
-console.log(color);
