7679eb0be882a178bec794b7c4309bcd09f58917
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-30 22:40:56 +0500

    For of Loop

--- Loops & Strings..js
+++ Loops & Strings..js
@@ -1,23 +1,29 @@
 
-let sum = 0;
-for (let i=0;i<5;i++)
-{
-  sum = sum + i;
-}
-console.log(sum);
+// let sum = 0;
+// for (let i=0;i<5;i++)
+// {
+//   sum = sum + i;
+// }
+// console.log(sum);
 
-let j=0;
-while(j<5)
-{
-    console.log(j);
-    j++;
-}
+// let j=0;
+// while(j<5)
+// {
+//     console.log(j);
+//     j++;
+// }
 
-let k=0;
-do {
-console.log(k);
-k++;
-}while(k<5);
+// let k=0;
+// do {
+// console.log(k);
+// k++;
+// }while(k<5);
+
+let str = "Amir Khan";
+for(let l of str)
+{
+    console.log(l);
+}
 
 
 
