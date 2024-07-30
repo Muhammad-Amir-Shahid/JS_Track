22c305118afed3e3f2fd0edbbce0ee9eb8843b8a
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-30 21:17:43 +0500

    Conditional Statements

--- conditional statement.js
+++ conditional statement.js
@@ -1,49 +1,47 @@
-// //conditional statement
-// let age=25;
-// if (age>=18)
-// {
-//     console.log("can vote");
-// }
+//conditional statement
+let age=25;
+if (age>=18)
+{
+    console.log("can vote");
+}
 
-// let result=age>18 ? "Yes":"No";  
-// console.log(result);
+let result=age>18 ? "Yes":"No";  
+console.log(result);
 
-// let mode = "dark";
-// let color;
-// if (mode==="dark")
-// {
-//     color ="black";
-// }
-// else if (mode==="light")
-// {
-//     color="white";
-// }
-// else
-// {
-// color ="Blue";
-// }
-// console.log(color);
-// const expr="orange";
-// switch(expr)
-// {
-// case 'orange':
-//     {
-//         console.log("Orange");
-//         break;
-//     }
-// case "Potato":
-//     {
-//         break;
-//     }
-// case "Egg":
-//     {
-//         break;
-//     }
-// }
+let mode = "dark";
+let color;
+if (mode==="dark")
+{
+    color ="black";
+}
+else if (mode==="light")
+{
+    color="white";
+}
+else
+{
+color ="Blue";
+}
+console.log(color);
+const expr="orange";
+switch(expr)
+{
+case 'orange':
+    {
+        console.log("Orange");
+        break;
+    }
+case "Potato":
+    {
+        break;
+    }
+case "Egg":
+    {
+        break;
+    }
+}
 
-alert("Warning!");
 
-let name=prompt("Enter Name");
-console.log(name);
+
 
 
