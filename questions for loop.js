0fc8043ae22b682a9c8f5c3ce937e973ff8914a2
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-30 23:09:00 +0500

    practice questions

--- questions for loop.js
+++ questions for loop.js
@@ -1,3 +1,4 @@
+//print even numbers from 0 to 100
 let num;
 for (let i=1;i<=100;i++)
 {
@@ -6,3 +7,14 @@ if (i%2===0)
     console.log(i);
 }
 }
+
+//guess the game number
+
+let number=13;
+let userNum=prompt("Guess the Number");
+
+while(number!=userNum)
+{
+    userNum=prompt("Guess Again!");
+}
+console.log("Congrats!");
