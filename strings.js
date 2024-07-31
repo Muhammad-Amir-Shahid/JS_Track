75ae4cb6470a1264df967bfa09200948d2b7be91
Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-31 23:11:33 +0500

    strings

--- strings.js
+++ strings.js
@@ -1,22 +1,22 @@
-// //strings
-// let str='Amir Khan';
+//strings
+let str='Amir Khan';
 
-// console.log(str[3]);
+console.log(str[3]);
 
-// //templates
+//templates
 
-// let str1=`this s template`;
-// console.log(str1);
+let str1=`this s template`;
+console.log(str1);
 
 
-// //use of template 
-// let obj={
-//     pen:"Pelikan",
-//     price:120
-// };
+//use of template 
+let obj={
+    pen:"Pelikan",
+    price:120
+};
 
-// let output=`The price of ${obj.pen} is ${obj.price} rupees.`;
-// console.log(output);
+let output=`The price of ${obj.pen} is ${obj.price} rupees.`;
+console.log(output);
 
 let str2 = "Bahria University Lahore Campus";
 
